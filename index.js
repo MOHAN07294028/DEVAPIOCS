e64399c47377b60df2afa8334fde2d81246ee9b1
MOHAN07294028 <nmoha4028@gmail.com>
2023-12-08 16:40:15 +0530

    render issue updated

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const dotenv = require('dotenv');
-const app = express();
-dotenv.config();
-app.use(cors());
-const registerModule = require('./module/register');
-const commonModule = require('./module/commonData');
-const loginModule = require('./module/login');
-const PORT = process.env.PORT || 3000; // Defaulting to 3000 if PORT is not set
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+const app = express()
+dotenv.config()
+app.use(cors())
+const registerModule = require('./module/register')
+const commonModule = require('./module/commonData')
+const loginModule = require('./module/login')
+const PORT = process.env.PORT
+app.use(express.json())
 
-app.use(express.json());
+app.use('/',registerModule)
+app.use('/common',commonModule)
+app.use('/',loginModule)
 
-// Serving static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
 
-// API routes
-app.use('/', registerModule);
-app.use('/common', commonModule);
-app.use('/', loginModule);
 
-// Handling all other routes to serve React app
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT,()=>console.log('Server is running '+PORT))
